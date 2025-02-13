3f2eabf48c83c54328ca49e54e0d657a44dcf069
RafaelCavalcantiMontenegro <rafacm2005@gmail.com>
2025-02-13 11:41:27 +0000

    variavel

---
+++ variável-C.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int n1 = 5;
+    double n2 = 3.1801;
+    char letra = 'a';
+    printf("n1 = %d\n", n1);
+    printf("n2 = %f\n", n2);
+    printf("letra = %c\n", letra);
+    return 0;
+}
